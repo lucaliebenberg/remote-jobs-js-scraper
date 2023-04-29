d80a7fa4e6f7d5afc90d91561d231cb4bb8b1baa
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-04-29 13:06:11 +0200

    updated scheduler

--- scheduler.js
+++ scheduler.js
@@ -3,7 +3,7 @@ const { CronJob } = require("cron");
 const remoteJobsScrapper = require("./index");
 
 console.log("Scheduler Started");
-const fetchRemoteJobsJob = new CronJob("* * * * *", async () => {
+const fetchRemoteJobsJob = new CronJob("0 0 * * *", async () => {
   console.log("Fetching new Remote Jobs...");
   await remoteJobsScrapper.run();
   // console.log("Jobs: ", jobs);
